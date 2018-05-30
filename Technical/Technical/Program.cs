f9cc77b31001ba2535c3f64fdf09bf628ac0f237
Alvin Nwachukwu <18370664+Valiantone1@users.noreply.github.com>
2018-05-30 14:15:32 -0500

    Add files via upload

---
+++ Technical/Technical/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Technical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            SAM sam = new SAM(20);
+
+            Console.WriteLine(sam.ToString());
+            Console.ReadKey();
+            
+
+
+        }
+
+        public class SAM{
+
+            private double calories, min = 0;
+            private int resultsTime = 0;
+
+
+            public SAM(int min){
+
+                //Constructor class
+                this.calories = 0;
+                this.min = min;
+                this.resultsTime = 0;
+            }
+
+            public SAM(int calories, int min, int resultsTime)
+            {
+                this.calories = calories;
+                this.min = min;
+                this.resultsTime = resultsTime;
+            }
+
+
+
+            //setters and getter
+            public double Calories { get => calories; set => calories = value; }
+            public double Min { get => min; set => min = value; }
+            public int ResultsTime { get => resultsTime; set => resultsTime = value; }
+
+
+            //calculate calories method
+            private void calculateCals() {
+
+                DateTime start = DateTime.Now;
+           
+                
+                calories = Math.Ceiling(
+                             Convert.ToDouble(
+                                 Decimal.Divide(200,15)) * min);
+
+                DateTime end = DateTime.Now;
+
+                TimeSpan span = end - start;
+
+                resultsTime = (int)span.TotalSeconds;
+            }
+
+           
+                    
+
+            public override string ToString()
+            {
+                calculateCals();
+                return  "You ran for " + min + " minutes.  You have burned " + calories + " calories \n The result returned " + resultsTime + " in seconds";
+            }
+
+
+
+        }
+
+    }
+}
---
+++ Technical/Technical/Technical.cs
@@ -0,0 +1,22 @@
+/********************************************************************
+#Create a voice assistant API for Calculating Calories Burned.
+#1. The API shall only respond to the name "SAM".
+#2. The API shall only respond to questions about calories.
+#3. The API shall only calculate calories burned 
+#     and the total time to process the result
+#4. Engineer shall validate input.
+#5. Engineer shall provide fully functional code with test.
+#6. The API shall take in a String of commands.
+#7. The API shall return a String:
+#8.   An example input will be : "Sam, I ran for 15 minutes.  
+# .             How many calories did I burn?.
+#9. Exact Output and Example:
+#     You ran for 15 minutes.  You have burned 200 calories.  
+#           The result returned in 3 seconds .
+
+#10.The output will be a string in plain English in this format: 
+#        "You ran for X minutes.  You have burned X calories.  
+#               The result returned in X seconds .
+#*******************************************************************************/
+
+
